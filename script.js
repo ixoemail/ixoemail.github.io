52e56c4d50945142814bfa5844807b5da849ce0d
ixoemail <hola.ixo.email@gmail.com>
2023-10-16 22:15:21 -0600

    oki

--- script.js
+++ script.js
@@ -217,4 +217,41 @@ class Slide extends React.Component {
 ReactDOM.render( /*#__PURE__*/React.createElement(Slideshow, null), document.getElementById("root"));
 
 
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
 
